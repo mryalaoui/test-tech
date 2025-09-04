2cdddfb6eff79a0f1a0620ba4b07c357385ad6f5
ALAOUI Yassine <yassine.alaoui@gmail.com>
2025-09-04 14:46:13 +0200

    init project arbre

---
+++ test-tech/src/main/java/com/test_tech/test_tech/TestTechApplication.java
@@ -0,0 +1,13 @@
+package com.test_tech.test_tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTechApplication.class, args);
+	}
+
+}
